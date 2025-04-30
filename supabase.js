4e3672ca3bdca91772e4eb4c0b534e33d4ca03d7
RsVodka <charly22mai@hotmail.fr>
2025-04-30 10:28:46 +0200

    In3

--- supabase.js
+++ supabase.js
@@ -53,6 +53,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("✅ Socket.IO prêt sur http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`✅ Socket.IO prêt sur http://localhost:${PORT}`);
 });
